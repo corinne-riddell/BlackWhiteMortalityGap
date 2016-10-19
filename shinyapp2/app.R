fd21c1ebcbb0dc391d56b930e7db3addebe51772
Corinne Riddell <corinne.riddell@mail.mcgill.ca>
2016-10-19 16:44:38 -0400

    beautifying a wee bi

--- shinyapp2/app.R
+++ shinyapp2/app.R
@@ -2,38 +2,49 @@ library(shiny)
 library(plotly)
 library(viridis)
 library(scales)
+library(shinythemes)
 load(file = "/Users/corinneriddell/Dropbox/BlackWhiteGap/Data/main_datasets.Rdata")
 
-ui1 <- fluidPage("Explore cause of death by age and year in the United States",
+ui1 <- fluidPage(theme = shinytheme("cosmo"),
+                 #shinythemes::themeSelector(), 
+                 titlePanel("Explore the black-white life expectancy gap in the United States"),
                  
-                 # *Input() functions,
-                 #selectInput(inputId = "sex", label = "Select a sex", choices = unique(levels(dat.clean.alabama$Sex2))),
-                 #selectInput(inputId = "race", label = "Select a race", choices = unique(levels(dat.clean.alabama$Race2))),
-                 #selectInput(inputId = "cod", label = "Select a cause of death", choices = unique(levels(dat.clean.alabama$COD2))),
-                 selectInput(inputId = "state", label = "Select a state", choices = unique(levels(dat.clean$State2))),
-                 selectInput(inputId = "year1", label = "Select first comparison year", choices = unique(dat.clean$Year3)),
-                 selectInput(inputId = "year2", label = "Select second comparison year", choices = unique(dat.clean$Year3)),
-                 #*Output() functions,
-                 #plotlyOutput("heatmapCOD"),
-                 #plotlyOutput("heatmapsCOD6")
-                 plotlyOutput("population_trend"),
-#add the new output object for our LE graphs -- see my notes.
-                 plotlyOutput("life_expectancy"),
-                 plotOutput("age_comp1"),
-                 plotOutput("age_comp2")
+                 sidebarLayout(
+                   sidebarPanel(width = 4,
+                     selectInput(inputId = "state", label = "Select a state", choices = unique(levels(dat.clean$State2))),
+                     strong("Select years to compare"),
+                     selectInput(inputId = "year1", label = NA, choices = unique(dat.clean$Year3), width = 100),
+                     selectInput(inputId = "year2", label = NA, choices = unique(dat.clean$Year3), selected = 2013, width = 100),
+                     strong("Legend\n"),
+                     img(src="legend.png"),
+                     strong("Population size over time"),
+                     plotlyOutput("population_trend")
+                      ),
+                   mainPanel(
+                     
+                     #add the new output object for our LE graphs -- see my notes.
+                     plotlyOutput("life_expectancy"),
+                     plotOutput("male_age_comp1"),
+                     plotOutput("male_age_comp2"),
+                     plotOutput("female_age_comp1"),
+                     plotOutput("female_age_comp2")                   )
+                 )
+
                  )
 
 server <- function(input, output) {
-  
+
   output$population_trend <- renderPlotly({
-      ggplotly(ggplot(subset(dat2, State2 == input$state & Age3 == 0), aes(y = Pop_across_age, x = Year3)) + 
+    p <- ggplotly(
+      ggplot(subset(dat2, State2 == input$state & Age3 == 0), aes(y = Pop_across_age, x = Year3)) + 
       geom_line(aes(col = Sex2, lty = Race2)) +
       scale_color_manual(values = c("#67a9cf", "#ef8a62")) +
       ylab("Population Size") + 
-      scale_y_continuous(label = scales::comma) + 
+      scale_y_continuous(label = comma) + 
       scale_x_continuous(name = "Year") + 
-      theme_minimal() + theme(legend.title=element_blank()))
-      
+      theme_minimal() + theme(legend.title=element_blank())) 
+      # p %>% layout(legend = list(x = 0.5, y = -2))
+     p %>% layout(showlegend = F)
    
   })
   
@@ -60,7 +71,7 @@ server <- function(input, output) {
     subplot(p1, p2, shareX = T, nrows = 2, titleY = T)
   })
   
-  output$age_comp1 <- renderPlot({
+  output$male_age_comp1 <- renderPlot({
     ggplot(as.data.frame(list.age.decomp.tables[[which(paired.ids$State2 ==  input$state & 
                                                          paired.ids$Year3 == input$year1 & 
                                                          paired.ids$Sex2 == "Male")]]), 
@@ -74,7 +85,7 @@ server <- function(input, output) {
       ggtitle(paste0("Contribution of age grouping to the black-white life expectancy gap for males in ", input$state, " in ", input$year1))
   })
 
-  output$age_comp2 <- renderPlot({
+  output$male_age_comp2 <- renderPlot({
   ggplot(as.data.frame(list.age.decomp.tables[[which(paired.ids$State2 ==  input$state & 
                                                        paired.ids$Year3 == input$year2 & 
                                                        paired.ids$Sex2 == "Male")]]), 
@@ -87,7 +98,36 @@ server <- function(input, output) {
     xlab("Loss or gain in life expectancy in years") +
     ggtitle(paste0("Contribution of age grouping to the black-white life expectancy gap for males in ", input$state, " in ", input$year2))
       
-})     
+})  
+  
+  output$female_age_comp1 <- renderPlot({
+    ggplot(as.data.frame(list.age.decomp.tables[[which(paired.ids$State2 ==  input$state & 
+                                                         paired.ids$Year3 == input$year1 & 
+                                                         paired.ids$Sex2 == "Female")]]), 
+           aes(y = Ages, x = C_x)) + 
+      geom_segment(aes(xend = 0, yend = Ages, col = adds_to_gap), lwd=8) +
+      scale_color_manual(values = c("#fddbc7", "#b2182b")) +  
+      theme_minimal() +
+      scale_y_reverse() + 
+      geom_segment(x = 0, xend = 0, y = 10, yend = -90) +
+      xlab("Loss or gain in life expectancy in years") +
+      ggtitle(paste0("Contribution of age grouping to the black-white life expectancy gap for males in ", input$state, " in ", input$year1))
+  })
+  
+  output$female_age_comp2 <- renderPlot({
+    ggplot(as.data.frame(list.age.decomp.tables[[which(paired.ids$State2 ==  input$state & 
+                                                         paired.ids$Year3 == input$year2 & 
+                                                         paired.ids$Sex2 == "Female")]]), 
+           aes(y = Ages, x = C_x)) + 
+      geom_segment(aes(xend = 0, yend = Ages, col = adds_to_gap), lwd=8) +
+      scale_color_manual(values = c("#fddbc7", "#b2182b")) +  
+      theme_minimal() +
+      scale_y_reverse() + 
+      geom_segment(x = 0, xend = 0, y = 10, yend = -90) +   
+      xlab("Loss or gain in life expectancy in years") +
+      ggtitle(paste0("Contribution of age grouping to the black-white life expectancy gap for males in ", input$state, " in ", input$year2))
+    
+  })  
     }
 
 shinyApp(ui = ui1, server = server)
