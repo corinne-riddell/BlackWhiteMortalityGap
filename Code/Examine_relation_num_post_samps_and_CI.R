14b970b3c5c13258809f8f5cf5b655fae769ef18
Kathryn <kt.morrison@mail.mcgill.ca>
2016-12-06 13:43:24 -0500

    investigate thinned CI

--- Code/Examine_relation_num_post_samps_and_CI.R
+++ Code/Examine_relation_num_post_samps_and_CI.R
@@ -1,38 +1,41 @@
+library(dplyr)
 #explore the relationship between the CI estimates and CI width as a function of the number of draws of the posterior
 #distribution.
 
-load("/Users/corinneriddell/Dropbox/BlackWhiteGap/Data/CI_vs_post_samples_results.RData")
-datasets <- c(georgia_2013_females[[3]], missouri_1985_females[[3]], utah_1969_females[[3]])
+#load("/Users/corinneriddell/Dropbox/BlackWhiteGap/Data/CI_vs_post_samples_results.RData") #LOCAL
+load("~/black_white_mortality_project/CI_vs_post_samples_results_thinned.RData") #SERVER
+# datasets <- c(georgia_2013_females[[3]], missouri_1985_females[[3]], utah_1969_females[[3]])
+datasets <- c(georgia_2013_females_thinned[[3]], missouri_1985_females_thinned[[3]], utah_1969_females_thinned[[3]])
 
-temp <- georgia_2013_females[[3]] 
+temp <- georgia_2013_females_thinned[[3]] 
 temp <- temp %>% mutate(CI_width_LEW = run_975_LEW - run_025_LEW,
                         CI_width_LEB = run_975_LEB - run_025_LEB,
                         CI_width_LEG = run_975_LEG - run_025_LEG)
 head(temp)
-georgia_2013_females[[3]] <- temp
+georgia_2013_females_thinned[[3]] <- temp
 
-temp <- missouri_1985_females[[3]] 
+temp <- missouri_1985_females_thinned[[3]] 
 temp <- temp %>% mutate(CI_width_LEW = run_975_LEW - run_025_LEW,
                         CI_width_LEB = run_975_LEB - run_025_LEB,
                         CI_width_LEG = run_975_LEG - run_025_LEG)
 head(temp)
-missouri_1985_females[[3]] <- temp
+missouri_1985_females_thinned[[3]] <- temp
 
-temp <- utah_1969_females[[3]] 
+temp <- utah_1969_females_thinned[[3]] 
 temp <- temp %>% mutate(CI_width_LEW = run_975_LEW - run_025_LEW,
                         CI_width_LEB = run_975_LEB - run_025_LEB,
                         CI_width_LEG = run_975_LEG - run_025_LEG)
 head(temp)
-utah_1969_females[[3]] <- temp
+utah_1969_females_thinned[[3]] <- temp
 
 #Georgia CI widths
-ggplot(georgia_2013_females[[3]][10:1000, ], aes(x = row, y = CI_width_LEW)) + geom_line() + 
-  geom_abline(intercept = georgia_2013_females[[3]]$CI_width_LEW[1000], slope = 0, lty = 2) + 
+ggplot(georgia_2013_females_thinned[[3]][10:100, ], aes(x = row, y = CI_width_LEW)) + geom_line() + 
+  geom_abline(intercept = georgia_2013_females_thinned[[3]]$CI_width_LEW[100], slope = 0, lty = 2) + 
   geom_line(aes(y = CI_width_LEB), col = "red") + 
-  geom_abline(intercept = georgia_2013_females[[3]]$CI_width_LEB[1000], slope = 0, lty = 2, col = "red")    
+  geom_abline(intercept = georgia_2013_females_thinned[[3]]$CI_width_LEB[100], slope = 0, lty = 2, col = "red")    
 
 ggplot(georgia_2013_females[[3]], aes(x = row, y = CI_width_LEG)) + geom_line(col = "blue") + 
-  geom_abline(intercept = georgia_2013_females[[3]]$CI_width_LEG[1000], slope = 0, lty = 2, col = "blue") 
+  geom_abline(intercept = georgia_2013_females[[3]]$CI_width_LEG[100], slope = 0, lty = 2, col = "blue") 
 
 #Missouri CI widths
 ggplot(missouri_1985_females[[3]], aes(x = row, y = CI_width_LEW)) + geom_line() + 
