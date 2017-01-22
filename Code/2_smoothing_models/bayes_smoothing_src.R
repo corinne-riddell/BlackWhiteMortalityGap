9da88ef2713c638eab8a8c7ebc9243d19935ac58
Kathryn <kt.morrison@mail.mcgill.ca>
2017-01-22 16:43:19 -0500

    generalized code for all states in males

--- Code/2_smoothing_models/bayes_smoothing_src.R
+++ Code/2_smoothing_models/bayes_smoothing_src.R
@@ -1,16 +1,15 @@
-
+library(purrr)
+library(ggplot2)
 library(rjags)
 library(R2jags) 
 library(devtools)
 #install_github(repo='johnbaums/jagstools') 
 library(jagstools)
-library(ggplot2)
 library(reshape2)
 library(parallel)
 library(gtools)
 library(bayesboot)
 library(coda)
-library(ggplot2)
 library(grid)
 library(gridExtra)
 
