cf2487fe43a6dd58dacfb6c064609e6df19f04f9
Kathryn <kt.morrison@mail.mcgill.ca>
2017-02-23 10:11:58 -0500

    three more

--- Code/run-models.R
+++ Code/run-models.R
@@ -1,13 +1,14 @@
 
-included.states <- c("Alabama", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
-"Delaware", "Florida", "Georgia", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
-"Louisiana", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",   
-"Missouri", "Nebraska", "Nevada", "New Jersey", "New Mexico", "New York",
-"North Carolina", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
-"South Carolina",  "Tennessee", "Texas", "Virginia", "Washington", "Washington DC",
-"West Virginia", "Wisconsin") 
-
-for(i in c(10, 12:16, 25)){
+for(i in c(12, 13, 15, 16, 25)){
+  
+  included.states <- c("Alabama", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+                       "Delaware", "Florida", "Georgia", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
+                       "Louisiana", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",   
+                       "Missouri", "Nebraska", "Nevada", "New Jersey", "New Mexico", "New York",
+                       "North Carolina", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
+                       "South Carolina",  "Tennessee", "Texas", "Virginia", "Washington", "Washington DC",
+                       "West Virginia", "Wisconsin") 
+  
   source("~/BlackWhiteMortalityGap/Code/entire_analysis.R")
   system.time(entire.analysis(state_i = included.states[i]))
   rm(list = ls(all = TRUE))
